8b3f928191c6082a237787d7dd67ec1357407668
Soumen3 <soumensamanta112233@gmail.com>
2024-11-06 22:54:48 +0530

    database change to default

--- coding/urls.py
+++ coding/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('problems/', views.problem_list_view, name='problem_list'),
     path('problem/<int:pk>/', views.problem_detail_view, name='problem_detail'),
     path('problem/<int:pk>/code_snippet/', views.get_code_snippet, name='get_code_snippet'),
-    path('post_problem/', views.post_problems, name='post_problem'),
+    path('post-problem/', views.post_problems, name='post_problem'),
 ]

b17b07f60f99da3d765e909ad57f61243c5ce983
Soumen3 <soumensamanta112233@gmail.com>
2024-11-25 21:34:12 +0530

    changes on home page design

--- Upgrade_Lab/settings.py
+++ Upgrade_Lab/settings.py
@@ -143,8 +143,8 @@ USE_L10N = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "static"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+# STATIC_ROOT = BASE_DIR / "static"
 
 
 # MEDIA_ROOT = BASE_DIR / "media"

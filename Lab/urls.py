d1bdbe322fbbbe7a5df038e8d74101e735e97b07
Soumen3 <soumensamanta112233@gmail.com>
2025-03-17 16:04:15 +0530

    social-media-add-done

--- Lab/urls.py
+++ Lab/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
         path('chatbot/', views.chatbot, name='chatbot'),
         path('user/<int:id>/<str:username>/', views.user_profile, name='user_profile'),
         path('user/<int:id>/<str:username>/add/', views.add_user_detail, name='add_user_detail'),
+        path('user/<int:id>/<str:username>/social-media/', views.update_social_media, name='update_social_media'),
         path('search/', views.search, name='search'),
-
 ]

08d15dcd57e595030b160f70e94867d4dfba18c2
Soumen3 <soumensamanta112233@gmail.com>
2025-03-23 23:49:37 +0530

    delete repo

--- vcs/admin.py
+++ vcs/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
 from .models import Repository, Commit, RepositoryFile
 
-admin.site.register(Repository)
-admin.site.register(Commit)
+# admin.site.register(Repository)
+@admin.register(Repository)
+class RepositoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'owner', 'created_at')
+    list_filter = ('owner',)
+    search_fields = ('name',)
+    readonly_fields = ('id', 'owner', 'created_at')
+
+# admin.site.register(Commit)
+@admin.register(Commit)
+class CommitAdmin(admin.ModelAdmin):
+    list_display = ('id', 'repository', 'message', 'created_at')
+    list_filter = ('repository',)
+    search_fields = ('message',)
+    readonly_fields = ('id', 'repository', 'message', 'created_at')
 
 
 @admin.register(RepositoryFile)
